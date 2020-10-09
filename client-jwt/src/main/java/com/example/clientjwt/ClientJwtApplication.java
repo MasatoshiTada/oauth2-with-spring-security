1ca8e390ce3292aab727f09534a0c5e37baea564
Masatoshi Tada <tada@casareal.co.jp>
2020-10-09 16:37:38 +0900

    各所にタイムアウト設定

--- client-jwt/src/main/java/com/example/clientjwt/ClientJwtApplication.java
+++ client-jwt/src/main/java/com/example/clientjwt/ClientJwtApplication.java
@@ -11,20 +11,33 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.security.oauth2.client.OAuth2AuthorizationFailureHandler;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.RemoveAuthorizedClientOAuth2AuthorizationFailureHandler;
+import org.springframework.security.oauth2.client.endpoint.DefaultRefreshTokenTokenResponseClient;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -66,18 +79,19 @@ public class ClientJwtApplication {
                                @Value("${resource-server.uri}") String resourceServerUri,
                                WebClientProperties webClientProperties,
                                ClientRegistrationRepository clientRegistrationRepository,
-                               OAuth2AuthorizedClientRepository authorizedClientRepository) {
-        // タイムアウトを設定
+                               OAuth2AuthorizedClientRepository authorizedClientRepository,
+                               RestTemplateBuilder restTemplateBuilder) {
+        // WebClientのタイムアウトを設定
         Function<? super TcpClient, ? extends TcpClient> tcpMapper = tcpClient -> {
-            // Connect Timeoutを500msに設定
+            // Connect Timeoutを設定
             return tcpClient.option(ChannelOption.CONNECT_TIMEOUT_MILLIS,
                     webClientProperties.getConnectTimeout())
                     .doOnConnected(conn -> conn
-                            // Read Timeoutを500msに設定
+                            // Read Timeoutを設定
                             .addHandlerLast(new ReadTimeoutHandler(
                                     webClientProperties.getReadTimeout(),
                                     TimeUnit.MILLISECONDS))
-                            // Write Timeoutを500msに設定
+                            // Write Timeoutを設定
                             .addHandlerLast(new WriteTimeoutHandler(
                                     webClientProperties.getWriteTimeout(),
                                     TimeUnit.MILLISECONDS))
@@ -85,10 +99,40 @@ public class ClientJwtApplication {
         };
         HttpClient httpClient = HttpClient.create()
                 .tcpConfiguration(tcpMapper);
-        // OAuth2関連の設定
+        // タイムアウトが設定されたRestTemplateの作成
+        RestTemplate restTemplate = restTemplateBuilder
+                .setConnectTimeout(Duration.ofMillis(1000)) // タイムアウト設定
+                .setReadTimeout(Duration.ofMillis(1000)) // タイムアウト設定
+                .messageConverters(new FormHttpMessageConverter(),
+                        new OAuth2AccessTokenResponseHttpMessageConverter())
+                .errorHandler(new OAuth2ErrorResponseErrorHandler())
+                .build();
+        // TokenResponseClientの作成
+        DefaultRefreshTokenTokenResponseClient accessTokenResponseClient =
+                new DefaultRefreshTokenTokenResponseClient();
+        accessTokenResponseClient.setRestOperations(restTemplate);
+        // OAuth2AuthorizedClientProviderの作成
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .authorizationCode()
+                        .refreshToken(refresh -> refresh.accessTokenResponseClient(accessTokenResponseClient))
+                        .build();
+        // OAuth2AuthorizedClientManagerの作成
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager =
+                new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+        // RemoveAuthorizedClientOAuth2AuthorizationFailureHandlerの作成
+        OAuth2AuthorizationFailureHandler authorizationFailureHandler =
+                new RemoveAuthorizedClientOAuth2AuthorizationFailureHandler(
+                        (clientRegistrationId, principal, attributes) ->
+                                authorizedClientRepository.removeAuthorizedClient(clientRegistrationId, principal,
+                                        (HttpServletRequest) attributes.get(HttpServletRequest.class.getName()),
+                                        (HttpServletResponse) attributes.get(HttpServletResponse.class.getName())));
+        // ServletOAuth2AuthorizedClientExchangeFilterFunctionの作成
         ServletOAuth2AuthorizedClientExchangeFilterFunction oAuth2Client =
-                new ServletOAuth2AuthorizedClientExchangeFilterFunction(
-                        clientRegistrationRepository, authorizedClientRepository);
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oAuth2Client.setAuthorizationFailureHandler(authorizationFailureHandler);
+        // WebClientを返す
         return builder.baseUrl(resourceServerUri)
                 // 作成したHttpClientを追加
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
--- client-jwt/src/main/java/com/example/clientjwt/security/config/SecurityConfig.java
+++ client-jwt/src/main/java/com/example/clientjwt/security/config/SecurityConfig.java
@@ -4,11 +4,21 @@ import com.example.clientjwt.security.keycloak.KeycloakService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,9 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
 
     private final KeycloakService keycloakService;
+    private final RestTemplateBuilder restTemplateBuilder;
 
-    public SecurityConfig(KeycloakService keycloakService) {
+    public SecurityConfig(KeycloakService keycloakService, RestTemplateBuilder restTemplateBuilder) {
         this.keycloakService = keycloakService;
+        this.restTemplateBuilder = restTemplateBuilder;
     }
 
     @Override
@@ -30,16 +42,65 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.oauth2Login()
-                .loginPage("/login")
-                .permitAll();
-        http.authorizeRequests()
-                .anyRequest().authenticated();
-        http.logout()
+        http.oauth2Login(oauth2 -> oauth2
+                .tokenEndpoint(token -> token
+                        // タイムアウト設定済みのDefaultAuthorizationCodeTokenResponseClientを指定
+                        .accessTokenResponseClient(accessTokenResponseClient())
+                ).userInfoEndpoint(userInfo -> userInfo
+                        // タイムアウト設定済みのDefaultOAuth2UserServiceを指定
+                        .userService(oAuth2UserService())
+                        // タイムアウト設定済みのOidcUserServiceを指定
+                        .oidcUserService(oidcUserService())
+                ).loginPage("/login")
+                .permitAll()
+        ).authorizeRequests(auth -> auth
+                .anyRequest().authenticated()
+        ).logout(logout -> logout
                 // 認可サーバーからもログアウトする
                 .addLogoutHandler(logoutFromAuthServer())
                 .invalidateHttpSession(true)
-                .permitAll();
+                .permitAll()
+        );
+    }
+
+    private DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient() {
+        RestTemplate restTemplate = restTemplateBuilder
+                // タイムアウトを設定
+                .setConnectTimeout(Duration.ofMillis(1000))
+                .setReadTimeout(Duration.ofMillis(1000))
+                // トークンエンドポイントからのレスポンスを解析するためのHttpMessageConverterを設定
+                .messageConverters(
+                        new FormHttpMessageConverter(),
+                        new OAuth2AccessTokenResponseHttpMessageConverter())
+                // トークンエンドポイントからのエラーレスポンスを解析するためのErrorHandlerを設定
+                .errorHandler(new OAuth2ErrorResponseErrorHandler())
+                .build();
+        DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient =
+                new DefaultAuthorizationCodeTokenResponseClient();
+        // タイムアウト設定済みのRestTemplateを設定
+        accessTokenResponseClient.setRestOperations(restTemplate);
+        return accessTokenResponseClient;
+    }
+
+    private DefaultOAuth2UserService oAuth2UserService() {
+        RestTemplate restTemplate = restTemplateBuilder
+                // タイムアウトを設定
+                .setConnectTimeout(Duration.ofMillis(1000))
+                .setReadTimeout(Duration.ofMillis(1000))
+                // /userinfoなどからのエラーレスポンスを解析するためのErrorHandlerを設定
+                .errorHandler(new OAuth2ErrorResponseErrorHandler())
+                .build();
+        DefaultOAuth2UserService oAuth2UserService = new DefaultOAuth2UserService();
+        // タイムアウト設定済みのRestTemplateを設定
+        oAuth2UserService.setRestOperations(restTemplate);
+        return oAuth2UserService;
+    }
+
+    private OidcUserService oidcUserService() {
+        OidcUserService oidcUserService = new OidcUserService();
+        // タイムアウト設定済みのOAuth2UserServiceを設定
+        oidcUserService.setOauth2UserService(oAuth2UserService());
+        return oidcUserService;
     }
 
     /**
